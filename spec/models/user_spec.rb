2a6a04a8c34bf5892d2caadea5068cfc5ea76115
Sam Mulube <sam@connectedenvironments.com>
2011-02-07 00:54:11 +0000

    Additional user model spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,12 @@ describe User do
     user.save
     user.reload
     user.username.should == "bob"
+  end
+
+  it "should crypt the secret if present" do
+    user = User.new(:username => "bob", :secret => "passwd")
+    user.save
+    user.reload
     user.crypted_secret.should_not be_nil
   end
 end
