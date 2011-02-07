5a84e5c5f2ecef0dc8ed1cc3a91cd50cb15eb175
Sam Mulube <sam@connectedenvironments.com>
2011-02-07 00:54:34 +0000

    An extra little rack spec, but only nibbling, not tackling any more significant code testing.

--- spec/rack/pachube_notifo_spec.rb
+++ spec/rack/pachube_notifo_spec.rb
@@ -11,4 +11,21 @@ describe Pachube::NotifoApp do
     get "/"
     last_response.should be_ok
   end
+
+  context "looking at registering users successfully" do
+    before(:each) do
+      User.stub!(:subscribe).and_return(Pachube::NOTIFO_OK_MESSAGE)
+      post "/users/register", { :username => "ibrahim" }
+    end
+
+    it "should redirect the user" do
+      last_response.status.should == 302 
+    end
+
+    it "should redirect to index page" do
+      follow_redirect!
+      last_request.url.should == "http://example.org/"
+      last_response.should be_ok
+    end
+  end
 end
